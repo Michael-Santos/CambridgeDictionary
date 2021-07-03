fb6c41fcaa205ea4b2466eedb079aad14a36f15e
Michael Santos <michael-santos@outlook.com.br>
2021-07-03 16:38:26 -0300

    Add FileManager to cache debug requests to files in Test project

---
+++ src/CambridgeDictionay.Console/FileManager.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace CambridgeDictionary.Cli.Test
+{
+    public class FileManager : IFileManager
+    {
+        const string basePath = "";
+        private readonly string _filePath;
+
+        public FileManager(string name)
+        {
+            _filePath = Path.Combine(basePath, name);
+        }
+
+        public void Create(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Read(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string name, string content)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/CambridgeDictionay.Console/IFileManager.cs
@@ -0,0 +1,11 @@
+﻿namespace CambridgeDictionary.Cli.Test
+{
+    public interface IFileManager
+    {
+        public void Create(string name);
+        public void Delete(string name);
+        public bool Exists(string name);
+        public string Read(string name);
+        public void Write(string name, string content);
+    }
+}
